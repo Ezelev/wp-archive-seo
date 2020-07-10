9ab5088ee3e66f3e01ea5707860f6bdc73eedd48
Ilya Ezelev <iiezelev@gmail.com>
2020-07-10 18:01:03 +0300

    Update archive-seo.php

--- archive-seo.php
+++ archive-seo.php
@@ -1,7 +1,20 @@
 <?php 
 
 /*
-Plugin Name: Archive Seo
-Description: Edit seo tags for arhive pages
+Plugin Name: Sample Plugin
+Description: This is a test plugin 
 */
+
+function get_custom_post_types() {
+
+	$args = array(
+		'_builtin' => false,
+		'has_archive' => true
+	);
+  $post_types = get_post_types( $args );
+  return $post_types;
+}
+
+add_action('init', 'get_custom_post_types');
+
 
